719302f376dca11cef9fe65ebe1fab551844b720
Charalambos Georgiades <cgeorgiades1@sheffield.ac.uk>
2022-05-05 10:46:25 +0100

    Made improvements to the robots movement and navigation

--- src/find_colour.py
+++ src/find_colour.py
@@ -3,6 +3,7 @@
 # Import the core Python modules for ROS and to implement ROS Actions:
 import rospy
 import time
+import numpy as np
 
 # Import some image processing modules:
 import cv2
@@ -14,14 +15,44 @@ from sensor_msgs.msg import Image
 # Import some other modules from within this package
 from tb3 import Tb3Move, Tb3Odometry
 
+#Import laserscan for wall following algorithm
+from sensor_msgs.msg import LaserScan
+from tf.transformations import euler_from_quaternion
+
 class colour_search(object):
+
+    def scan_callback(self, scan_data):
+        # From the front of the robot, obtain a 20 degree 
+        # arc of scan data either side of the x-axis 
+        left_arc = scan_data.ranges[0:91]
+        right_arc = scan_data.ranges[-90:]
+        # combine the "left_arc" and "right_arc" data arrays, flip them so that 
+        # the data is arranged from left (-20 degrees) to right (+20 degrees)
+        # then convert to a numpy array
+        self.front_arc = np.array(left_arc[::-1] + right_arc[::-1])
+        # Obtain the minimum distance measurement within the "front_arc" array
+        # to indicate when we are getting close to something up ahead:
+        self.min_distance = self.front_arc.min()
+
+        # Advanced:
+        # Create another numpy array which represents the angles 
+        # (in degrees) associated with each of the data-points in 
+        # the "front_arc" array above:
+        arc_angles = np.arange(-90, 91)
+        # determine the angle at which the minimum distance value is located
+        # in front oself.arc_anglesf the robot:
+        self.object_angle = arc_angles[np.argmin(self.front_arc)]
 
     def __init__(self):
         node_name = "turn_and_face"
         rospy.init_node(node_name)
 
+        self.min_distance = 100
+        self.front_arc = np.empty(180)
+
         self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw",
             Image, self.camera_callback)
+        self.sub_scan = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
         self.cvbridge_interface = CvBridge()
         
 
@@ -48,15 +79,18 @@ class colour_search(object):
         # For making sure we run the code only at the start
         self.target_found = False
         self.target_colour = -1
+        self.seeing_beacon = False
+        self.ready_to_beaconing = False
 
         self.m00 = 0
         self.m00_min = 10000
+        self.target_angle = 0
 
     def shutdown_ops(self):
         self.robot_controller.stop()
         cv2.destroyAllWindows()
         self.ctrl_c = True
-    
+
     def camera_callback(self, img_data):  
         try:
             cv_img = self.cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8")
@@ -74,11 +108,11 @@ class colour_search(object):
         cropped_img = cv_img[crop_z0:crop_z0+crop_height, crop_y0:crop_y0+crop_width]
 
         hsv_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
+        # Colours Thresholds       Turquoise         Red       Yellow           Green           Purple             Blue
+        lower_threshold = [(86, 150, 100),(0, 190, 100), (26,98,100),   (57,150,100), (148, 250, 100), (118, 215, 100)]
+        upper_threshold = [(93, 250, 255),  (10, 255, 255),(34,251,255), (63, 255, 255), (153, 275, 255), (123, 253, 255)]
 
         if self.target_found and self.target_colour == -1:
-            # Colours Thresholds       Turquoise         Red       Yellow           Green           Purple             Blue
-            lower_threshold = [(86, 150, 100),(0, 190, 100), (26,98,100),   (57,150,100), (148, 250, 100), (118, 215, 100)]
-            upper_threshold = [(93, 250, 255),  (10, 255, 255),(34,251,255), (63, 255, 255), (153, 275, 255), (123, 253, 255)]
             
             index = 0
             while self.target_found and self.target_colour == -1 :
@@ -94,6 +128,24 @@ class colour_search(object):
                     self.target_colour = index
                     print("empika dames")
                 index += 1
+            
+        elif self.ready_to_beaconing:
+            colour_index = self.target_colour
+            mask = cv2.inRange(hsv_img, lower_threshold[colour_index], upper_threshold[colour_index]) 
+
+            m = cv2.moments(mask)
+                
+            self.m00 = m["m00"]
+            self.cy = m["m10"] / (m["m00"] + 1e-5)
+
+            if self.m00 > self.m00_min:
+                    cv2.circle(hsv_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
+                    print("efkalato foto re paithkia")
+                    self.seeing_beacon = True
+                    self.target_angle = self.cy
+            else:
+                print("En to vlepo")
+                self.seeing_beacon = False
 
 
     #     #Blue pillar bounds
@@ -136,7 +188,7 @@ class colour_search(object):
                 print("I am turning right")
                 self.robot_controller.set_move_cmd(0, -1.57)
                 self.robot_controller.publish()
-                time.sleep(2)
+                time.sleep(1)
                 self.robot_controller.set_move_cmd(0, 0)
                 self.robot_controller.publish()
                 
@@ -149,12 +201,65 @@ class colour_search(object):
                 print("I am turning left")
                 self.robot_controller.set_move_cmd(0, 1.57)
                 self.robot_controller.publish()
-                time.sleep(2)
+                time.sleep(1)
                 self.robot_controller.set_move_cmd(0, 0)
-                self.robot_controller.publish()  
+                self.robot_controller.publish() 
 
-            # print(self.robot_odom.posx)
-            print("Colour found: ", self.target_colour)
+                print("Colour found: ", self.target_colour) 
+                time.sleep(1)
+                self.ready_to_beaconing = True
+                print ("kamno beaconing")
+
+            
+
+            min_right_side = np.amin(self.front_arc[120:150])
+            min_front_side = np.amin(self.front_arc[75:105])
+
+            if self.seeing_beacon:
+                if min_front_side < 0.45 and self.seeing_beacon:
+                    self.robot_controller.set_move_cmd(0, 0)
+                    self.robot_controller.publish()
+                    print("We fucking did it")
+                    self.ctrl_c = True
+                else:
+                    print ("vlepo to tzai pao")
+                    #vriski to kentro damesa
+                    if self.cy >= 250 and self.cy <= 450:
+                        self.robot_controller.set_move_cmd(0.26, 0)
+                        self.robot_controller.publish()
+                    if self.cy < 250:
+                        self.robot_controller.set_move_cmd(0.2, 0.30)
+                        self.robot_controller.publish()
+                    elif self.cy > 450:
+                        self.robot_controller.set_move_cmd(0.2, -0.30)
+                        self.robot_controller.publish()
+
+
+                    #self.robot_controller.set_move_cmd(0.26, 0)
+                    #self.robot_controller.publish()
+
+            elif (min_front_side < 0.45):
+                self.robot_controller.set_move_cmd(0, 0)
+                self.robot_controller.publish()
+                self.robot_controller.set_move_cmd(0, 1.0)
+                self.robot_controller.publish()
+
+            else:        
+                if(min_right_side > 0.3 and min_right_side < 0.4):
+                    self.robot_controller.set_move_cmd(0.26, 0)
+                    self.robot_controller.publish()
+                elif(min_right_side < 0.3):
+                    self.robot_controller.set_move_cmd(0.26, 0.9)
+                    self.robot_controller.publish()
+                elif(min_right_side > 0.4):
+                    self.robot_controller.set_move_cmd(0.26, -0.9)
+                    self.robot_controller.publish()
+
+
+
+
+
+
             self.rate.sleep()
             
 if __name__ == '__main__':
